849a88ab6fb6422bd3818e5230653565fc4ae788
Bhudati Vineela <Bhudati.Vineela@godigit.com>
2024-07-26 15:08:35 +0530

    AddressBookMain

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class AddressBookMain {
+import java.util.Scanner;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner((System.in));
+        AddressBook addressbook=new AddressBook();
+        System.out.println("Enter the address book details:");
+
+        System.out.println("Enter FirstName:");
+        String FirstaName=sc.nextLine();
+
+        System.out.println("Enter SecondName");
+        String SecondName=sc.nextLine();
+
+        System.out.println("Enter Your Address");
+        String Address=sc.nextLine();
+
+        System.out.println("Enter Your City");
+        String City=sc.nextLine();
+
+        System.out.println("Enter Your State");
+        String State=sc.nextLine();
+
+        System.out.println("Enter Your Zip");
+        String Zip=sc.nextLine();
+
+        System.out.println("Enter Your PhoneNumber");
+        String PhoneNumber=sc.nextLine();
+
+        System.out.println("Enter Your Email");
+        String Email=sc.nextLine();
+
+        Contact newContact=new Contact(FirstaName,SecondName,Address,City,State,Zip,PhoneNumber,Email);
+        addressbook.addContact(newContact);
+        System.out.println("Contact Added Successfully!!");
+        System.out.println("Current contacts i n address book");
+        addressbook.displayContacts();
+    }
 }
