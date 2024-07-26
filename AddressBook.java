505f1e602c4eb5fd2ed7d6f8cdd55bb23194c571
Bhudati Vineela <Bhudati.Vineela@godigit.com>
2024-07-26 15:07:33 +0530

    AddressBook

--- AddressBook.java
+++ AddressBook.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class AddressBook {
+import java.util.ArrayList;
+import java.util.List;
+public class AddressBook {
+    public List<Contact> contacts;
+    public AddressBook(){
+        this.contacts=new ArrayList<>();
+
+    }
+    public void addContact(Contact contact){
+        contacts.add(contact);
+
+    }
+    public void displayContacts(){
+        for(Contact contact:contacts){
+            System.out.println(contact);
+        }
+    }
+
 }
