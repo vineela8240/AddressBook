40c23456eb2d3f2e13a64309ba8339e0bff3a70b
Bhudati Vineela <Bhudati.Vineela@godigit.com>
2024-07-27 10:56:53 +0530

    This contains the main method to delete the Contact in AddressBook

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -8,8 +8,9 @@ public class AddressBookMain {
         while (true) {
             System.out.println("1. Add Contact");
             System.out.println("2. Edit Contact");
-            System.out.println("3. Display Contacts");
-            System.out.println("4. Exit");
+            System.out.println("3. Delete Contact");
+            System.out.println("4. Display Contacts");
+            System.out.println("5. Exit");
             System.out.print("Choose an option: ");
             int choice = scanner.nextInt();
             scanner.nextLine();  // Consume newline
@@ -85,13 +86,29 @@ public class AddressBookMain {
                         System.out.println("Contact not found.");
                     }
                     break;
-
                 case 3:
+                    System.out.println("Enter the name of the contact to delete:");
+
+                    System.out.print("First Name: ");
+                    String deleteFirstName = scanner.nextLine();
+
+                    System.out.print("Last Name: ");
+                    String deleteLastName = scanner.nextLine();
+
+                    boolean isDeleted = addressBook.deleteContact(deleteFirstName, deleteLastName);
+                    if (isDeleted) {
+                        System.out.println("Contact deleted successfully!");
+                    } else {
+                        System.out.println("Contact not found.");
+                    }
+                    break;
+
+                case 4:
                     System.out.println("Current contacts in address book:");
                     addressBook.displayContacts();
                     break;
 
-                case 4:
+                case 5:
                     System.out.println("Exiting...");
                     scanner.close();
                     return;
