8b375569c123c4593eeaf21776729e0d63aca149
Bhudati Vineela <Bhudati.Vineela@godigit.com>
2024-07-27 10:25:49 +0530

    Ability to edit existing contact person using their name

--- AddressBook.java
+++ AddressBook.java
@@ -1,19 +1,38 @@
 import java.util.ArrayList;
 import java.util.List;
+
 public class AddressBook {
-    public List<Contact> contacts;
-    public AddressBook(){
-        this.contacts=new ArrayList<>();
+    private List<Contact> contacts;
 
+    public AddressBook() {
+        this.contacts = new ArrayList<>();
     }
-    public void addContact(Contact contact){
+
+    public void addContact(Contact contact) {
         contacts.add(contact);
-
     }
-    public void displayContacts(){
-        for(Contact contact:contacts){
+
+    public void displayContacts() {
+        for (Contact contact : contacts) {
             System.out.println(contact);
         }
     }
 
+    public Contact findContactByName(String firstName, String lastName) {
+        for (Contact contact : contacts) {
+            if (contact.getFirstName().equalsIgnoreCase(firstName) && contact.getLastName().equalsIgnoreCase(lastName)) {
+                return contact;
+            }
+        }
+        return null;
+    }
+
+    public void editContact(Contact contact, String address, String city, String state, String zip, String phoneNumber, String email) {
+        contact.setAddress(address);
+        contact.setCity(city);
+        contact.setState(state);
+        contact.setZip(zip);
+        contact.setPhoneNumber(phoneNumber);
+        contact.setEmail(email);
+    }
 }
